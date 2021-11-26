58962525bf6c71fda23b08ac67a282abb2867715
andrei-filip912 <filip.andrei912@gmail.com>
2021-11-26 15:27:56 +0100

    installed npm pack, eslint fix env.dev.js, update movies.js
    -added axios in package.json for posting the file
    - getting the token with auth0hook,
    sending the file through formData object

--- src/views/movies.js
+++ src/views/movies.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React,	{useState, useEffect} from 'react';
 
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@mui/material';
 import Loading from '../components/Loading';
-
+import axios from 'axios';
 
 const Movies = () => {
+	const[token, setToken] = React.useState();
+	const { getAccessTokenSilently } = useAuth0();
+	const { user } = useAuth0();
+
+	useEffect(async () => {
+		const stringToken = 'Bearer ' + await getAccessTokenSilently({
+			audience: 'https://express.sample',
+			scope: 'read:current_user'
+		});
+		setToken(stringToken);
+	}, []);
+
 	const {loading} = useAuth0();
 	if(loading){
 		return (<Loading/>);
 	}
 
+	// console.log(user);
+
 	function openFileDialog(){
 		let input = document.createElement('input');
 		input.type = 'file';
-		input.accept = 'video/mp4,video/*';
+		// input.accept = 'video/mp4,video/*';
 		input.onchange = _ => {
-			let files =   Array.from(input.files);
-			console.log(files); //do something with the files
+			const file = Array.from(input.files)[0];
+			var formData = new FormData();
+			formData.append('image', file);
+
+			axios.post(
+				'http://localhost:4000/upload',
+				formData,
+				{
+					headers : {
+						'Content-Type': 'multipart/form-data; boundary=--X--',
+						'Authorization': token,
+					}
+				}).then(res => console.log(res))
+				.catch(err => console.log(err));
 		};
 		input.click();
 	}
@@ -32,7 +58,7 @@ const Movies = () => {
 		} else {
 		// Use DataTransfer interface to access the file(s)
 		}
-		console.log(ev.dataTransfer.items[0]);
+		// console.log(ev.dataTransfer.items[0]);
 	}
 
 	function dragOverHandler(ev) {
