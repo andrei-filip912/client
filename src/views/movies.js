1ab3dd85686f6174f0a9805a60466737db305324
andrei-filip912 <filip.andrei912@gmail.com>
2021-11-26 16:36:56 +0100

    refactor movies.js
    -moved sending to api in a method
    -removed console.log

--- src/views/movies.js
+++ src/views/movies.js
@@ -23,49 +23,52 @@ const Movies = () => {
 		return (<Loading/>);
 	}
 
-	// console.log(user);
 
 	function openFileDialog(){
 		let input = document.createElement('input');
 		input.type = 'file';
-		// input.accept = 'video/mp4,video/*';
+		input.accept = 'video/mp4,video/*';
+
 		input.onchange = _ => {
 			const file = Array.from(input.files)[0];
-			var formData = new FormData();
-			formData.append('image', file);
-
-			axios.post(
-				'http://localhost:4000/upload',
-				formData,
-				{
-					headers : {
-						'Content-Type': 'multipart/form-data; boundary=--X--',
-						'Authorization': token,
-					}
-				}).then(res => console.log(res))
-				.catch(err => console.log(err));
+			console.log(file);
+			sendToApi(file);
 		};
 		input.click();
 	}
 	function dropHandler(ev) {
-		console.log('File(s) dropped');
-
 		// Prevent default behavior (Prevent file from being opened)
 		ev.preventDefault();
 
 		if (ev.dataTransfer.items) {
-			// Use DataTransferItemList interface to access the file(s)
-		} else {
-		// Use DataTransfer interface to access the file(s)
+			// Use DataTransferItemList interface to access the file
+			const file = ev.dataTransfer.items[0].getAsFile();
+			sendToApi(file);
 		}
-		// console.log(ev.dataTransfer.items[0]);
 	}
 
 	function dragOverHandler(ev) {
-		console.log('File(s) in drop zone');
-
 		// Prevent default behavior (Prevent file from being opened)
 		ev.preventDefault();
+	}
+
+	async function sendToApi(file) {
+		// create formData object for the request body and attach the file to it
+		var formData = new FormData();
+		formData.append('image', file);
+
+		// sending the post request o the upload api
+		return axios.post(
+			'http://localhost:4000/upload',
+			formData,
+			{
+				headers : {
+					'Content-Type': 'multipart/form-data; boundary=--X--',
+					'Authorization': token,
+				}
+			})
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
 	}
 
 	return (
