5141d1f77279bee7b7cc93508f24d58967f8ccf7
andrei-filip912 <filip.andrei912@gmail.com>
2022-11-10 00:36:37 +0100

    added Grid with videos

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Card, CardMedia, CardContent, Typography, Grid } from '@material-ui/core';
 import VideoCard from './VideoCard';
+import { Box } from '@material-ui/core';
 
 const MovieList = ({ movies }) => {
 
@@ -15,11 +16,22 @@ const MovieList = ({ movies }) => {
 		setHtml(
 			movies.map(element => {
 				return (
-					<VideoCard 
+					<Grid
 						key={element._id}
-						url={element.url}
-						name={element.name}
-					/>
+						item
+						xs={12}
+						sm={6}
+						md={4}
+					>
+						<div>
+							<VideoCard
+
+								url={element.url}
+								name={element.name}
+							/>
+						</div>
+					</Grid>
+
 				);
 			})
 		);
@@ -27,7 +39,13 @@ const MovieList = ({ movies }) => {
 
 	return (
 		<div>
-			{html}
+			<Grid
+				container
+				spacing={3}
+				style={{paddingLeft: '40px', paddingRight: '40px'}}
+			>
+				{html}
+			</Grid>
 		</div>
 	);
 };
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
-import { Card, CardMedia, CardContent, Typography, Grid } from '@material-ui/core';
+import { Card, CardMedia, CardContent, Typography } from '@material-ui/core';
 /* eslint-disable */
 
 function VideoCard({url, name }) {
     return (
         <Card
             variant="outlined"
-            sx={{ width: '34%' }}
         >
             <CardMedia>
                 <ReactPlayer
                     url={url}
                     controls={true}
                     width='100%'
+                    heigh='100%'
                 />
             </CardMedia>
             <CardContent>
-                <Typography variant="title" color="text.primary">
+                <Typography>
                     {name}
                 </Typography>
             </CardContent>
